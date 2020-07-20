0a1f4e91d0586af391d6582a9017aee11f3067a7
Sheva Athalla <shevanejiwolop@gmail.com>
2020-07-20 23:00:14 +0700

    oop rev 2

--- OOP (Day 1 Week 1)/index.php
+++ OOP (Day 1 Week 1)/index.php
@@ -3,7 +3,7 @@ require_once('Elang.php');
 require_once('Harimau.php');
 
 $elang = new Elang('elang_1', 2, 'terbang tinggi', 10, 5);
-$harimau = new Harimau('harimau_1', 4, 'lari cepat', 7, 5);
+$harimau = new Harimau('harimau_1', 4, 'lari cepat', 7, 8);
 
 echo $elang->serang($harimau);
 echo $elang->diserang($harimau);
